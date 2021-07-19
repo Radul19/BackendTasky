36a535b2cba3aa29651c39f04758b5acb8d99e61
Radul19 <radulito19@gmail.com>
2021-07-19 07:53:05 -0400

    commit 4

--- src/database.js
+++ src/database.js
@@ -1,11 +1,15 @@
 const { Pool } = require('pg')
 
 const config = {
-    user: 'postgres',
-    host: 'localhost',
-    password: '123123',
-    database: 'my_first_DB'
+    connectionString: process.env.DATABASE_URL,
+    user: 'bhtghgpppmqyod',
+    host: 'ec2-23-21-4-7.compute-1.amazonaws.com',
+    password: '45c31e0e878c1906e76b0f1b4e156b28096feff184b9d882f4c01acff4d629f3',
+    database: 'd76sjvbr38fa7b',
+    port: 5432,
+    ssl: { rejectUnauthorized: false }
 }
+
 
 
 const pool = new Pool(config)
