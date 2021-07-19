ac2c7ae55ab496df0619e441a2c1f96bc8e90aa5
Radul19 <radulito19@gmail.com>
2021-07-19 09:04:10 -0400

    commit 5

--- src/database.js
+++ src/database.js
@@ -1,17 +1,27 @@
 const { Pool } = require('pg')
+require('dotenv').config()
 
-const config = {
-    connectionString: process.env.DATABASE_URL,
-    user: 'bhtghgpppmqyod',
-    host: 'ec2-23-21-4-7.compute-1.amazonaws.com',
-    password: '45c31e0e878c1906e76b0f1b4e156b28096feff184b9d882f4c01acff4d629f3',
-    database: 'd76sjvbr38fa7b',
-    port: 5432,
-    ssl: { rejectUnauthorized: false }
+// const config = {
+//     user: 'postgres',
+//     host: 'postgres://bhtghgpppmqyod:45c31e0e878c1906e76b0f1b4e156b28096feff184b9d882f4c01acff4d629f3@ec2-23-21-4-7.compute-1.amazonaws.com:5432/d76sjvbr38fa7b',
+//     password: '123123',
+//     database: 'my_first_DB'
+// }
+// const devConfig = {
+//     user:  process.env.PG_USER,
+//     password: process.env.PG_PASSWORD,
+//     host: process.env.PG_HOST,
+//     port: process.env.PG_PORT,
+//     database: process.env.PG_DATABASE,
+// }
+const proConfig = {
+    connectionString: process.env.DATABASE_URL
 }
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 
-
-
-const pool = new Pool(config)
+// const pool = new Pool(devConfig)
+const pool = new Pool({
+    connectionString: process.env.NODE_ENV === 'production' ? proConfig : devConfig
+})
 
 module.exports = pool
